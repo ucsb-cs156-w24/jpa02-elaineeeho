e67f902ceba7af20b00eb72f71445c3d0bf20c33
Elaine Ho <elaine_ho@ucsb.edu>
2024-01-18 11:17:30 -0800

    Update HelloController.java to be net_id instead of csil id

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from elaineeeho!";
+        return "Greetings from elaine_ho!";
     }
     
 }
